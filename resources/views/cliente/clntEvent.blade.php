bc9abc86258c81f5057a08a7ef17e393ce1790ef
Meneses-11 <125163230+Meneses-11@users.noreply.github.com>
2023-03-06 22:53:29 -0600

    Update clntEvent.blade.php

--- resources/views/cliente/clntEvent.blade.php
+++ resources/views/cliente/clntEvent.blade.php
@@ -1,5 +1,24 @@
 @extends('plantillas.menuCliente')
 @section('contenido')
-<h1>Eventos</h1>
-<h2>Modificado</h2>
+<div class="contPadre">
+    <div class="contEvent">
+        <div>
+            <h1 class="titEvent">Mis eventos</h1>
+            <button>Crear Evento</button>
+        </div>
+        <div class="eventos">
+            <div class="evento"><h1>Evento 1</h1></div>
+            <div class="evento"><h1>Evento 2</h1></div>
+            <div class="evento"><h1>Evento 3</h1></div>
+            <div class="evento"><h1>Evento 4</h1></div>
+            <div class="evento"><h1>Evento 5</h1></div>
+            <div class="evento"><h1>Evento 6</h1></div>
+            <div class="evento"><h1>Evento 7</h1></div>
+            <div class="evento"><h1>Evento 8</h1></div>
+        </div>        
+    </div>
+    <div class="contDesc">
+
+    </div>
+</div>
 @endsection
