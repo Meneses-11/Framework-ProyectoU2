eeb547c74230aa872fa446950cd5bdae5f23aa4a
Kevin Tovilla <kevitotovilla6@gmail.com>
2023-03-11 20:55:41 -0600

    Actualizacion Kevin ClnPaquetesTerm

--- resources/views/cliente/clntPaquetes.blade.php
+++ resources/views/cliente/clntPaquetes.blade.php
@@ -9,12 +9,12 @@
             <div class="info-producto">
                 <h2>Paquete bodas</h2>
                 <p class="Descripcion">Decoramos el salon conforme a lo que estas buscando </p>
-               <!-- <button class="añade-carrito">Añadir al carrito </button>-->
+               <!-- <button class="añade-carrito">Cotizar</button>-->
             </div>
         </div>
         <div class="item">
             <figure>
-                <img src="img\fiesta-adultos.jpg" alt="travis1">
+                <img src="img\fiestas-adultos.jpg" alt="travis1">
             </figure>
             <div class="info-producto">
                 <h2>Paquete Cumpleaños</h2>
@@ -36,22 +36,22 @@
         </div>
         <div class="item">
             <figure>
-                <img src="img\salon-vacio" alt="travis2">
+                <img src="img\Vacio.jpg" alt="travis2">
             </figure>
             <div class="info-producto">
-                <h2>Salon eventos libre</h2>
-                <p class="Descripcion">El salon se renta vacio solo con sillas y mesas
+                <h2>Salon eventos vacio</h2>
+                <p class="Descripcion">El salon se renta vacio
                      sin ningun tipo de decoracion </p>
                 <!-- <button class="añade-carrito">Añadir al carrito </button>-->
             </div>
         </div>
         <div class="item">
             <figure>
-                <img src="img\tenis55.jpg" alt="off">
+                <img src="img\salon-vacio-decoracion.jpg" alt="off">
             </figure>
             <div class="info-producto">
-                <h2>Salon eventos Fiestas libre</h2>
-                <p class="Descripcion">El salon se renta con decoraciones libres</p>
+                <h2>Salon eventos Fiestas libre con decoraciones</h2>
+                <p class="Descripcion">El salon se renta con decoraciones libres y con sillas y mesas</p>
                 <!-- <button class="añade-carrito">Añadir al carrito </button>-->
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,12 @@ use App\Http\Controllers\entradaController;
     //return view('/plantillas/menuCliente');
     //return view('welcome');
     return view('/cliente/clntEvent');
-});*/
+});
+
+public function verPantallainicio(){
+        return view('cliente.anPinicio');
+    }
+    */
 
 Route::get('/gerente/usuarios',function(){
     return view('/gerente/inicio');
@@ -23,4 +28,3 @@ Route::get('/error',[entradaController::class,'error'])->name('error');
 Route::get('verPaquetes', [clienteController::class, 'verPaquetes'])->name('paquetes');
 Route::get('verEventos', [clienteController::class, 'verEventos'])->name('eventos');
 Route::get('verInformacion', [clienteController::class, 'verInformacion'])->name('informacion');
-
