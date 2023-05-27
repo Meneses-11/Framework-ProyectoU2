0a8f9a8da726c915674154390be4919c68f16a9d
adrian manuel meneses lopez <ameneses11lml@gmail.com>
2023-05-26 19:41:59 -0600

    Adrian-- prueba animacion paquetes

--- resources/views/cliente/clntPaquetes.blade.php
+++ resources/views/cliente/clntPaquetes.blade.php
@@ -28,11 +28,29 @@
                         <a href="{{ route('paquete.detalle', $paquete->id_paquete) }}" style="text-decoration: none; color:black;">
                             <div class="item">
                                 <figure>
-                                    <div class="slyder">
-                                        @foreach ($paquete->imagenes as $item)
-                                            <li><img src={{ $item->ruta }} alt={{ $item->nombre }}></li>
-                                        @endforeach
+                                    <div class="slider">
+                                        <ul class="slider-list">
+                                            @foreach ($paquete->imagenes as $item)
+                                                <li><img src={{ $item->ruta }} alt={{ $item->nombre }}></li>
+                                            @endforeach
+                                        </ul>
                                     </div>
+
+                                    <script>
+                                        // Obtener el número de imágenes
+                                        const imageCount = document.querySelectorAll('.slider-list li').length;
+
+                                        // Establecer la variable CSS --image-count en el elemento .slider-list si hay más de una imagen
+                                        if (imageCount > 1) {
+                                            document.documentElement.style.setProperty('--image-count', imageCount);
+                                        } else {
+                                            document.documentElement.style.setProperty('--image-count', 2); // Establecer un mínimo de 2 para que se mueva aunque solo haya una imagen
+                                            //document.documentElement.style.setProperty('--image-count', 1);
+                                            const sliderList = document.querySelector('.slider-list');
+                                            sliderList.style.animation = 'none'; // Desactivar la animación si solo hay una imagen
+                                        }
+                                    </script>
+
                                     <div class="capa" style="margin-top: -12.5rem;">
                                         <p class="Descripcion">{{ $paquete->descripcion }}</p>
                                     </div>
