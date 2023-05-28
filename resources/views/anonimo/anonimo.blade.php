ecf8893d6fab215d098bbdf21c98a22567339b40
Kevin Tovilla <kevitotovilla6@gmail.com>
2023-05-28 00:15:32 -0600

    slider actualizacion

--- resources/views/anonimo/anonimo.blade.php
+++ resources/views/anonimo/anonimo.blade.php
@@ -25,20 +25,27 @@ Salón de Eventos
         @foreach ($paquetes as $paquete)
         <a href="{{ route('login') }}" style="text-decoration: none; color:black;">
             <div class="item">
-                <figure>
-                    @foreach ($paquete->imagenes as $item)
-                    <img src={{$item->ruta }} alt="bodas">
 
-                    @endforeach
-                    <div class="capa" style="margin-top: -12.5rem;">
-                        <p class="Descripcion">{{ $paquete->descripcion }} </p>
-                    </div>
-                </figure>
+
+                    <figure>
+                        <div class="slider">
+                            @foreach ($paquete->imagenes as $item)
+                                <div class="slide" >
+                                    <img src={{$item->ruta }} alt="bodas" style= "margin: 0">
+                                </div>
+                            @endforeach
+                        </div>
+
+                        <div class="capa" style="margin-top: -12.5rem;">
+                            <p class="Descripcion">{{ $paquete->descripcion }} </p>
+                        </div>
+                    </figure>
+
+
                 <div class="info-producto">
                     <h2>{{ $paquete->nombre }}</h2>
                     <!--<p class="Descripcion">{{ $paquete->descripcion }} </p> -->
                    <!-- <button class="añade-carrito">Cotizar</button>-->
-
                 </div>
             </div>
         </a>
@@ -46,5 +53,50 @@ Salón de Eventos
 
     </div>
  </div>
+
 @endsection
+
+@section('scripts')
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+  var sliders = document.querySelectorAll('.slider');
+
+  sliders.forEach(function(slider) {
+    var slides = slider.querySelectorAll('.slide');
+    var slideIndex = 0;
+
+    function showSlide() {
+      slides[slideIndex].classList.add('active');
+    }
+
+    function hideSlide() {
+      slides[slideIndex].classList.remove('active');
+    }
+
+    function nextSlide() {
+      hideSlide();
+      slideIndex++;
+      if (slideIndex >= slides.length) {
+        slideIndex = 0;
+      }
+      showSlide();
+    }
+
+    function resetSlider() {
+      hideSlide();
+      slideIndex = 0;
+      showSlide();
+    }
+
+    showSlide();
+    setInterval(nextSlide, 2500);
+    setInterval(resetSlider, slides.length * 2500);
+  });
+});
+</script>
+@endsection
+
+
+
+
 
